11d9dc31f0b2f8603e3f07b1414cffa04bc85cfb
Radosław Białowąs <radoslaw.bialowas@gmail.com>
2018-05-21 13:02:49 +0200

    testing architecture

--- how_to_keras/main.py
+++ how_to_keras/main.py
@@ -27,12 +27,16 @@ def conv_model(input_shape, classes):
     # Define the input placeholder as a tensor with shape input_shape. Think of this as your input image!
     X_input = Input(input_shape)
 
-    # Zero-Padding: pads the border of X_input with zeroes
-    X = ZeroPadding2D((3, 3))(X_input)
-
     # CONV -> BN -> RELU Block applied to X
-    X = Conv2D(32, (7, 7), strides=(1, 1), name='conv0')(X)
+    X = Conv2D(32, (5, 5), strides=(1, 1), padding='same', name='conv0')(X_input)
     X = BatchNormalization(axis=3, name='bn0')(X)
+    X = Activation('relu')(X)
+
+    # MAXPOOL
+    X = MaxPooling2D((2, 2), name='max_pool')(X)
+
+    X = Conv2D(25, (3, 3), strides=(1, 1), padding='same', name='conv1')(X_input)
+    X = BatchNormalization(axis=3, name='bn1')(X)
     X = Activation('relu')(X)
 
     # MAXPOOL
@@ -40,7 +44,8 @@ def conv_model(input_shape, classes):
 
     # FLATTEN X (means convert it to a vector) + FULLYCONNECTED
     X = Flatten()(X)
-    X = Dense(classes, activation='sigmoid', name='fc')(X)
+    X = Dense(1024, activation='relu', name='fc0')(X)
+    X = Dense(classes, activation='sigmoid', name='fc1')(X)
 
     # Create model. This creates your Keras model instance, you'll use this instance to train/test the model.
     model_ = Model(inputs=X_input, outputs=X, name='model')
@@ -51,6 +56,11 @@ def conv_model(input_shape, classes):
 model = conv_model(X_train.shape[1:], classes)
 model.compile(optimizer=optimizers.Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x=X_train, y=Y_train, epochs=1, batch_size=64)
+epochs = 1
 
-print(model.summary())
+model.save('model_epochs-' + str(epochs))
+
+if __name__ == '__main__':
+    print(model.summary())
+    model.fit(x=X_train, y=Y_train, epochs=epochs, batch_size=512)
+    print(model.summary())
